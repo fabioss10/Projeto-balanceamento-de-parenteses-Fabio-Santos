e966ac5d02379e6beb0db1061206c59905948da7
fabioss10 <fabioss10@hotmail.com>
2015-06-16 17:13:19 -0300

    BalanceamentoParenteses

---
+++ PackageTmp/Views/Home/Index.aspx
@@ -0,0 +1,30 @@
+﻿<%@ Page Language="C#" Inherits="System.Web.Mvc.ViewPage<dynamic>" %>
+
+<!DOCTYPE html>
+
+<html>
+<head runat="server">
+    <title>Fábio Santos</title>
+    <script src="../../Scripts/jquery-1.5.1.min.js" type="text/javascript"></script>
+    <script>
+        $(document).ready(function () {
+            $("#btnCheck").click(function () {
+                $.ajax({
+                    url: "Home/CheckString",
+                    method: "POST",
+                    data: { str: $("#str").val() },
+                    success: function (data) {
+                        $("#result").html(data);
+                    }
+                });
+            });
+        });
+    </script>
+</head>
+<body>
+    <div>
+        <h1>Digite a String:<input type="text" id="str" name="str"/><input type="button" id="btnCheck" /></h1>
+    </div>
+    <div id="result"></div>
+</body>
+</html>
---
+++ fonts/Controllers/HomeController.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Fabio_Santos.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        //[HttpPost]
+        public ActionResult CheckString()
+        {
+            String str = Request["str"];
+            return Content("<b>" + balanceamentoParenteses(str) + "</b>");
+        }
+
+        [NonAction]
+        private bool balanceamentoParenteses(String str)
+        {
+            bool c = false;
+            int i = 0;
+            int contAbre = 0;
+            int contFecha = 0;
+            bool procuraFecha = false;
+            foreach (char value in str)
+            {
+                if (value.Equals('('))
+                {
+                    String palavra = str.Substring(i + 1, str.Length-(i+1));
+                    procuraFecha = true;
+                    c = balanceamentoParenteses(palavra);
+                    //break;
+                    contAbre++;
+                    
+                }
+                else if (value.Equals(')')&&procuraFecha==true)
+                {
+                    contFecha++;
+                    procuraFecha = false;
+                    //return true;
+                }
+                i++;
+
+            }
+
+
+
+            if (contAbre == contFecha)
+            {
+                return true;
+            }
+            else {
+                return false;
+            }
+            
+        }
+    }
+}
